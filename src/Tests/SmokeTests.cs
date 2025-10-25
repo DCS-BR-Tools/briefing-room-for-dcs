d19a95328c2023f227afd2ca0d09f76e41a47e93
john681611 <john681611@hotmail.com>
2025-10-25 11:11:10 +0100

    Test: Auto test all maps

--- src/Tests/SmokeTests.cs
+++ src/Tests/SmokeTests.cs
@@ -5,15 +5,29 @@ namespace BriefingRoom4DCS.Tests;
 [Collection("Sequential")]
 public class SmokeTests
 {
-    [Fact]
-    public void SingleMission()
+    [Theory]
+    [InlineData("Afghanistan")]
+    [InlineData("Caucasus")]
+    [InlineData("Falklands")]
+    [InlineData("GermanyCW")]
+    [InlineData("Iraq")]
+    [InlineData("Kola")]
+    [InlineData("MarianaIslands")]
+    [InlineData("MarianaIslandsWWII")]
+    [InlineData("Nevada")]
+    [InlineData("Normandy")]
+    [InlineData("PersianGulf")]
+    [InlineData("SinaiMap")]
+    [InlineData("Syria")]
+    [InlineData("TheChannel")]
+    public void SingleMission(string theaterID)
     {
         var briefingRoom = new BriefingRoom();
 
-        DCSMission mission = briefingRoom.GenerateMission($"{BriefingRoom.GetBriefingRoomRootPath()}\\Default.brt");
+        DCSMission mission = briefingRoom.GenerateMission($"{BriefingRoom.GetBriefingRoomRootPath()}\\testTemplates\\{theaterID}.brt");
 
         Assert.NotNull(mission);
-        Assert.Equal("Caucasus", mission.TheaterID);
+        Assert.Equal(theaterID, mission.TheaterID);
         Assert.NotNull(mission.Briefing.Name);
         Assert.NotNull(mission.Briefing.Description);
     }
