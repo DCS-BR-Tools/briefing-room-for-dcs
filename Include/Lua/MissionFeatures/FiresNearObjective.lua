9f8941a63ef16ee14dab1ad6f3a7f6b533969ef7
john681611 <john681611@hotmail.com>
2026-01-03 19:12:17 +0000

    Fix:  new lines in scripts missing

--- Include/Lua/MissionFeatures/FiresNearObjective.lua
+++ Include/Lua/MissionFeatures/FiresNearObjective.lua
@@ -6,4 +6,3 @@ for i=math.random(4, 15),1,-1 do
   local spawnPoint = { x = unitVec2.x, y = land.getHeight(unitVec2), z = unitVec2.y }
   trigger.action.effectSmokeBig(spawnPoint, math.random(1, 4), math.random(7, 10) * 0.1)
 end
-
