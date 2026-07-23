ebde9b018b62efc992fff293e7f44075613adb31
john681611 <john681611@hotmail.com>
2026-07-23 09:29:03 +0100

    Updated:  DCS Data 2.9.28.26283

--- src/BriefingRoom/BriefingRoom.cs
+++ src/BriefingRoom/BriefingRoom.cs
@@ -46,7 +46,7 @@ namespace BriefingRoom4DCS
 
         public const string BUILD_VERSION = "20260213-171541";
 
-        public const string TARGETED_DCS_WORLD_VERSION = "2.9.25.21123";
+        public const string TARGETED_DCS_WORLD_VERSION = "2.9.28.26283";
 
         public const int MAXFILESIZE = 50000000;
 
