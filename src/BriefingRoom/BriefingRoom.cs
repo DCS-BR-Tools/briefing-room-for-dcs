f27fd059b6a0975635906044cacfee7135ec03b3
john681611 <john681611@hotmail.com>
2026-08-03 10:56:18 +0100

    CI: Somehow Build number got hardcoded.

--- src/BriefingRoom/BriefingRoom.cs
+++ src/BriefingRoom/BriefingRoom.cs
@@ -44,7 +44,7 @@ namespace BriefingRoom4DCS
 
         public const string VERSION = "0.5.~RELEASE_VERSION~";
 
-        public const string BUILD_VERSION = "20260213-171541";
+        public const string BUILD_VERSION = "~BUILD_VERSION~";
 
         public const string TARGETED_DCS_WORLD_VERSION = "2.9.28.26283";
 
